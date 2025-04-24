bfe913d7bb085a605fced73071f6fb4468321552
Manuel Anzola <manuel.anzola31@gmail.com>
2025-04-24 17:33:02 -0400

    5 commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://rhxirhzykldmjvkuxieg.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoeGlyaHp5a2xkbWp2a3V4aWVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4ODYzNDMsImV4cCI6MjA1OTQ2MjM0M30.R4IqEr-7KzkFg459ENvaUwXIQ6gaaqAAHhaodUNdfnU',
+    url: 'SUPABASE_URL',
+    anonKey: 'ANON_KEY',
   );
 
   runApp(App());
